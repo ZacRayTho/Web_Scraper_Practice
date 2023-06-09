7872268ad1aaca4c37d52636520102b80dbd11e2
unknown <zacraytho@gmail.com>
2023-06-09 18:10:18 -0400

    have search template working

--- WebScraper/web_scraping_requests.py
+++ WebScraper/web_scraping_requests.py
@@ -1,10 +1,22 @@
-from bs4 import BeautifulSoup
-import requests
 import csv
+from bs4 import BeautifulSoup
 from selenium import webdriver
 
 driver = webdriver.Chrome()
-url = 'https://www.oreillyauto.com/'
+url = "https://www.oreillyauto.com"
+# driver.get(url)
+# input()
+
+def get_url(search_term):
+    # Generate a url from search term
+    template = "https://www.oreillyauto.com/search?q={}"
+    search_term = search_term.replace(" ", "+")
+    return template.format(search_term)
+# https://www.oreillyauto.com/shop/b/bearings---seals/wheel-bearings-and-seals/wheel-bearing/5426c6f5d17c?q=wheel+bearing
+url = get_url('wheel bearing')
+print(url)
+
 driver.get(url)
-# doc = BeautifulSoup(result.text, 'html.parser')
-# print(doc.prettify())
+
+
+
