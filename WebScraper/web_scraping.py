3c0eeb317857558c46b84b8748035007b2bd807f
ZacRayTho <zachary.thomas@student.awesomeinc.org>
2023-06-09 19:19:13 +0000

    practice parsing html in same repo

---
+++ WebScraper/web_scraping.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+
+with open("./WebScraper/index.html", "r") as f:
+    # open syntax, first parameter is the file to open, second parameter is the mode 'r' means read mode
+    # as f, f stands for file
+    doc = BeautifulSoup(f, "html.parser")
+    # beautfulSoup syntax, first parameter is document you want to read, and second parameter is the type of parser
+
+# find_all makes a list of all tags with that tag name, can be accessed like a list/array 
+# tag = doc.find_all("p")[1]
+# you can grab nested tags too!
+# print(tag.b)
+
+# how to get a certain tag
+# tag = doc.title
+# tag.string will give only inside the tag
+# print(tag)
+
+# print(doc.prettify())
+# .prettify() will format the html
